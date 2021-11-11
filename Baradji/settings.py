5c290cf3ffbda90f98eab45454efd22439fb488b
Hydra16LeGrand <baradjiamara17@gmail.com>
2021-11-11 07:27:05 +0000

    ajout du nom de domaine ivoirestore.herokuapp.com

--- Baradji/settings.py
+++ Baradji/settings.py
@@ -32,7 +32,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'baradjise-shop.herokuapp.com', 'baradjise-shop.com', 'www.baradjise-shop.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ivoirestore.herokuapp.com']
 
 
 # Application definition
